d3fbf7c3a484eded591725f9c24a55edae34f05d
冉文杰 <ranwenjie@didichuxing.com>
2016-03-07 20:01:55 +0800

    Initial Commit

---
+++ AttributeStringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AttributeStringDemo
+//
+//  Created by ranwenjie on 16/3/7.
+//  Copyright © 2016年 ranwenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
