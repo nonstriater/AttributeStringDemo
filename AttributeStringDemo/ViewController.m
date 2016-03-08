c94a7f0fc67adc05c3a075696a6b9068e1678c25
冉文杰 <ranwenjie@didichuxing.com>
2016-03-08 11:50:22 +0800

    first ci

--- AttributeStringDemo/ViewController.m
+++ AttributeStringDemo/ViewController.m
@@ -17,7 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self addButton];
 }
+
+- (void)addButton{
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, 40, self.view.frame.size.width, 200);
+    //btn.backgroundColor = [UIColor blueColor];
+    
+    NSString *text1 = @"搜索服务开小差了";
+    NSString *text2 = @"\n请选择以下一个地点作为起点或点此重试点此重试";
+
+    NSMutableAttributedString *attributeTitle1 = [[NSMutableAttributedString alloc] initWithString:text1 attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:16],NSForegroundColorAttributeName:[UIColor grayColor]}];
+     NSMutableAttributedString *attributeTitle2 = [[NSMutableAttributedString alloc] initWithString:text2 attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:12],NSForegroundColorAttributeName:[UIColor redColor]}];
+    [attributeTitle1 appendAttributedString:attributeTitle2];
+    [btn setAttributedTitle:attributeTitle1 forState:UIControlStateNormal];
+    btn.titleLabel.numberOfLines = 2;
+    btn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    
+    [self.view addSubview:btn];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
